4b261d95c7287881bf0a06d5dae9237edc690b6a
Kenrique <ngwakenri2016@gmail.com>
2023-06-19 08:30:00 +0100

    print_comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,6 +7,7 @@
  */
 
 int main(void)
+
 {
 	int x;
 	int y;
@@ -21,18 +22,29 @@ int main(void)
 				if (x < y && y < z)
 				{
 					putchar(x + '0');
+
 					putchar(y + '0');
+
 					putchar(z + '0');
-                    
+
 					if (x + y + z != 24)
+
 					{
 						putchar(',');
+
 						putchar(' ');
 					}
+
 				}
+
 			}
+
 		}
+
 	}
+
 	putchar('\n');
+
 	return (0);
+
 }
