0e6a3034c7227813c9bb94ba7d6e652db23f0ecb
Kenrique <ngwakenri2016@gmail.com>
2023-06-20 08:18:44 +0100

    putchar.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main - printing out _putchar using printf
+* Description: using putchar to print _putchar
+* Return: 0 Success
+*/
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
