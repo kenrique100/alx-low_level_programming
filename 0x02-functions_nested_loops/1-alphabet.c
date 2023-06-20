13609450a2f3d5577bbfdc5d900f4520bda3785e
Kenrique <ngwakenri2016@gmail.com>
2023-06-20 11:38:37 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_alphabet - This function prints the alphabets in lowercase followed by a new line
+*
+* Return: Success 0
+*/
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}	_putchar('\n');
+}
