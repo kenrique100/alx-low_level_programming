b65b0cea634669a7bec7b4f5f407f251a1d0f70a
Kenrique <ngwakenri2016@gmail.com>
2023-06-22 11:46:23 +0100

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - prints integers from 0 - 9
+* followed by a new line
+* Return: Always 0
+*/
+void print_numbers(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
