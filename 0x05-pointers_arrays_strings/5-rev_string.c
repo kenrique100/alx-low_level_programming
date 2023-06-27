3d0d2528431fe1b00f5f659fe526091ba1387ea1
Kenrique <ngwakenri2016@gmail.com>
2023-06-27 22:02:04 +0100

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,17 @@
 */
 void rev_string(char *s)
 {
-int i, l;
-char c;
-for (l = 0; s[l] != '\0'; l++)
-for (i = 0; i < l / 2; i++)
+char rev = s[0];
+int counter = 0;
+int i;
+
+while (s[counter] != '\0')
+counter++;
+for (i = 0 ; i < counter ; i++)
 {
-c = s[i];
-s[i] = s[l - 1 - i];
-s[l - 1 - i] = c;
+counter--;
+rev = s[i];
+s[i] = s[counter];
+s[counter] = rev;
 }
 }
