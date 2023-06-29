bf3090b119704d9e28a473bbac80feb9043e11e8
Kenrique <ngwakenri2016@gmail.com>
2023-06-29 17:49:57 +0100

    reverse a spring

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* reverse_array - function to revers an array
+*
+* @a: array input parameter
+*
+* @n: number of elements of array
+*
+* Return: always 0 (Success)
+*/
+void reverse_array(int *a, int n)
+{
+int i, temp, e;
+
+i = 0;
+e = n - 1;
+while (i < e)
+{
+temp = a[i];
+a[i] = a[e];
+a[e] = temp;
+i++;
+e--;
+}
+}
