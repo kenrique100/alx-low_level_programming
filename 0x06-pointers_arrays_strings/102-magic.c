f9860ad862ef5a908ba4621342c70f91523dee52
Kenrique <ngwakenri2016@gmail.com>
2023-06-30 00:38:43 +0100

    prints magic numbers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
-* main - print magic numbers
+* main - print ot magic numbers
 *
-* Return: always 0
+* Return: 0
 */
 
 int main(void)
@@ -13,15 +14,16 @@ int *p;
 
 a[2] = 1024;
 p = &n;
+
 /*
 * write your line of code here...
-* Remember:
+* Remember
 * - you are not allowed to use a
 * - you are not allowed to modify p
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-*(p + 5) = 98;
+p[5] = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
