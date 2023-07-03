0a5371b47cff709b2389e26679998d21c9b018f8
Kenrique <ngwakenri2016@gmail.com>
2023-07-03 20:44:54 +0100

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,11 @@
 *
 * Return: a pointer with values in bytes
 */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
+int i = 0;
+
+for (; n > 0 ; i++)
 {
 s[i] = b;
 n--;
