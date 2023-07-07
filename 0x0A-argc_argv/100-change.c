9bc6d1470b71e9d85fd322bc96fb6feefb711d68
Kenrique <ngwakenri2016@gmail.com>
2023-07-07 16:39:15 +0100

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,61 +1,43 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
+
 /**
-* checker - checks for valid input
-* @argc: argument count
-* @i: counter for argv[]
-* @j: counter for argv[][]
-* @argv: argument vector
-* Return: 0 on success, 1 on failure
+* main - function prints the minimum number of coins to make
+* change for an amount of money.
+*
+* @argc: number of command line arguments.
+* @argv: array that contains the program command line arguments.
+*
+* Return: 0 - success.
 */
-int checker(int argc, int i, unsigned int j, char *argv[])
-{
-for (i = 1; i <= argc; i++)
-for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-if (isdigit(argv[i][j]) == 0)
-return (1);
-return (0);
-}
-/**
-* main - Prints the minimum number of coins
-* to make change for an amount of cents.
-* @argc: argument count
-* @argv: argument vector
-* Return: 0 on success
-*/
+
 int main(int argc, char *argv[])
 {
-unsigned int cents;
-int coins;
+int cents, coins = 0;
 
-cents = coins = 0;
-if (argc == 2)
-{
-if (argv[1][0] == '-')
-{
-printf("0\n");
-if (checker(argc, 1, 0, argv) == 0)
-{
-cents = atoi(argv[1]);
-for ( ; cents >= 25; coins++, cents -= 25)
-;
-for ( ; cents >= 10; coins++, cents -= 10)
-;
-for ( ; cents >= 5; coins++, cents -= 5)
-;
-for ( ; cents >= 2; coins++, cents -= 2)
-;
-for ( ; cents >= 1; coins++, cents--)
-;
-printf("%d\n", coins);
-}
-}
-}
-else
+if (argc == 1 || argc > 2)
 {
 printf("Error\n");
+return (1);
 }
+
+cents = atoi(argv[1]);
+
+while (cents > 0)
+{
+if (cents >= 25)
+cents -= 25;
+else if (cents >= 10)
+cents -= 10;
+else if (cents >= 5)
+cents -= 5;
+else if (cents >= 2)
+cents -= 2;
+else if (cents >= 1)
+cents -= 1;
+coins += 1;
+}
+printf("%d\n", coins);
 return (0);
 }
