4ffa561021031ee40d354f8b43e8c0ddb51fc914
Kenrique <ngwakenri2016@gmail.com>
2023-07-07 15:47:13 +0100

    program prints its own name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - a program prints its own name.
- * @argc: argument count
- * @argv: argument vectors
- * Return: 0 success
- */
+* main - a program prints its own name.
+* @argc: argument count
+* @argv: argument vectors
+* Return: 0 success
+*/
 int main(int argc, char *argv[])
 {
-	argc--; /*added to pass checks*/
-	printf("%s\n", argv[0]);
-	return (0);
+argc--; /*added to pass checks*/
+printf("%s\n", argv[0]);
+return (0);
 }
