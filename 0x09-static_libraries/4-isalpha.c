87c5bca78d8443889b7285857c9237be77a4ce0f
Kenrique <ngwakenri2016@gmail.com>
2023-07-07 14:13:26 +0100

    function that checks for alphabets

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-* _isalpha - function that checks for alphabets
-*
-* @c: parameter to be checked
-*
-* Return: 1 if it's an alphabet,
-* 0 otherwise if it's not an alphabet
-*/
-int _isalpha(int c)
-{
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+ * _isalpha - function that checks for alphabets
+ *
+ * @c: parameter to be checked
+ *
+ * Return: 1 if it's an alphabet,
+ * 0 otherwise if it's not an alphabet
+ */
+ int _isalpha(int c)
+ {
+ if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
 }
