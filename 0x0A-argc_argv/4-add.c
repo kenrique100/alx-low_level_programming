e380c2ff846dd200e4a41ccd082d8205b355750a
Kenrique <ngwakenri2016@gmail.com>
2023-07-07 16:14:50 +0100

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,51 +1,30 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
-#include <string.h>
-#include "main.h"
+#include <ctype.h>
 
 /**
-* checker - function checks for valid input
-*
-* @argc: argument count
-* @i: counter for argv[]
-* @j: counter for argv[][]
-* @argv: argument vector
-*
-* Return: 0 on success, 1 on failure
-*/
-
-int checker(int argc, int i, unsigned int j, char *argv[])
-{
-for (i = 1; i <= argc; i++)
-for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-if (isdigit(argv[i][j]) == 0)
-{
-return (1);
-}
-return (0);
-}
-/**
-* main - function adds all arguments together if they are digits.
-*
-* @argc: argument count only accepts ints separated by spaces.
-* @argv: argument vector
-*
-* Return: 0 on success, 1 on failure
+* main - adds positive numbers.
+* @argc: number of command line arguments.
+* @argv: array that contains the program command line arguments.
+* Return: 0 - success.
 */
 
 int main(int argc, char *argv[])
 {
-int result, i;
+int i, j, add = 0;
 
-result = 0;
-if (checker(argc, 1, 0, argv) == 1)
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
 {
 printf("Error\n");
 return (1);
 }
-for (i = 1; i < argc; i++)
-result += atoi(argv[i]);
-printf("%d\n", result);
+}
+add += atoi(argv[i]);
+}
+printf("%d\n", add);
 return (0);
 }
