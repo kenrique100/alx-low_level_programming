1a958d9ef2ca7b7ab11b9576434338ecc212c9a5
Kenrique <ngwakneri2016@gmail.com>
2023-07-10 09:17:40 +0100

    main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,6 +8,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
-int count_word(char *str, int pos, char headchar);
+int count_word(char *str);
 
 #endif
