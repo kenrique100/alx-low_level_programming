037a40cb5e02b9dd65081d5517a7fd44553be5fa
Kenrique <ngwakneri2016@gmail.com>
2023-07-10 09:15:51 +0100

    splits a string into a 2-D array of words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -19,19 +19,19 @@
 
 int word_count(char *str, int pos, char headchar)
 {
-int i, word_count, count, flag;
+int i, wordcount, count, flag;
 
-str[0] != ' ' ? (word_count = 1) : (word_count = 0);
+str[0] != ' ' ? (wordcount = 1) : (wordcount = 0);
 for (i = 0, flag = 0; str[i]; i++)
 {
 if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0' && flag == 0)
 {
-word_count++;
+wordcount++;
 flag = 1;
 }
-if (pos > 0 && pos == word_count)
+if (pos > 0 && pos == wordcount)
 {
-if (pos > 0 && pos == word_count && headchar > 0)
+if (pos > 0 && pos == wordcount && headchar > 0)
 return (i);
 for (count = 0; str[i + count + 1] != ' '; count++)
 ;
@@ -40,7 +40,7 @@ return (count);
 if (str[i] == ' ')
 flag = 0;
 }
-return (word_count);
+return (wordcount);
 }
 /**
 * strtow - splits a string into a 2-D array of words
