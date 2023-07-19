bdca3a3b5aeb0ec29d6b7b8007d4479d7610abe5
Kenrique <ngwakneri2016@gmail.com>
2023-07-19 17:47:36 +0100

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+* print_name - function to print a name
+*
+* @name: string input
+* @f: pointer to a function casted to char
+*
+* Return: nothing
+*/
+void print_name(char *name, void(*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+f(name);
+}
