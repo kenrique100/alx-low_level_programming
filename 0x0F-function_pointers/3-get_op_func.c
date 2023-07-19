9948ccffee213882f0086fc3dd26a9f49a509d82
Kenrique <ngwakneri2016@gmail.com>
2023-07-19 19:13:14 +0100

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stddef.h>
+#include <stdlib.h>
 /**
 * get_op_func - decides which math to use based on the operator
 *
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "3-calc.h"
+#include <string.h>
+#include <stdlib.h>
 /**
 * main - function declaration
 *
