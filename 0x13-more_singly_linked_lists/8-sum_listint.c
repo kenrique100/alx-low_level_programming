a9c1893bda98742784fdc1cdae9950d890fd8996
Kenrique <ngwakneri2016@gmail.com>
2023-07-31 07:07:07 +0100

    function that sums all elements of a list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* sum_listint - function that sums all elements of a list
+*
+* @head: point to the head of the list
+*
+* Return: sum of all elements
+*/
+int sum_listint(listint_t *head)
+{
+int sums;
+
+for (sums = 0; head; sums += head->n, head = head->next)
+;
+return (sums);
+}
