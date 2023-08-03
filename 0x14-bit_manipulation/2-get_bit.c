a7d3a38d8fb131b097bf6e4f1c860d8c4f2fec28
Kenrique <ngwakneri2016@gmail.com>
2023-08-03 16:32:21 +0100

    function that sets bit at index to 1

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,17 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* set_bit - function that sets bit at index to 1
+* set_bit - function that gets the value of int at index
 *
 * @n: pointer to number
 * @index: index to change
 *
 * Return: 1 on success, -1 on failure
 */
-int set_bit(unsigned long int *n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
-if (index > (8 * sizeof(*n)) - 1)
-return (-1);
-*n = *n | (1 << index);
-return (1);
+int bit;
+return ((index > (8 * sizeof(n) - 1)) ? -1 : (int)(n >> index) & 1);
 }
